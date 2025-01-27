a9f931cedf727ab80b0ebfa38e73eb50e81fad79
Soumadip_07 <soumadip17720@gmail.com>
2025-01-28 04:06:21 +0530

    Sign up and sign in page ui set

--- src/components/SignUp/LogIn.jsx
+++ src/components/SignUp/LogIn.jsx
@@ -3,6 +3,7 @@ import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../slice/AuthSlice';
 import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logo.png'
 
 export default function LogIn() {
     const dispatch = useDispatch();
@@ -41,50 +42,65 @@ export default function LogIn() {
 
     return (
         <section className="login-section p-5">
-            <form
-                onSubmit={formik.handleSubmit} // Correctly binding Formik's handleSubmit
-                className="d-flex flex-column justify-content-center align-items-center gap-4"
-            >
-                {/* Username (Email) */}
-                <label htmlFor="username" className="d-flex flex-column">
+            <div className="form-container d-flex justify-content-evenly align-content-center gap-5">
+                <form
+                    onSubmit={formik.handleSubmit} // Correctly binding Formik's handleSubmit
+                    className="d-flex flex-column justify-content-center align-items-center gap-2"
+                >
+                    {/* Username (Email) */}
+                    {/* <label htmlFor="username" className="d-flex flex-column">
                     Email
-                </label>
-                <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.username}
-                />
-                {formik.errors.username && formik.touched.username && (
-                    <div className="error">{formik.errors.username}</div>
-                )}
+                </label> */}
+                    <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.username}
+                        placeholder="Enter your email"
 
-                {/* Password */}
-                <label htmlFor="password" className="d-flex flex-column">
-                    Password
-                </label>
-                <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                />
-                {formik.errors.password && formik.touched.password && (
-                    <div className="error">{formik.errors.password}</div>
-                )}
+                    />
+                    {formik.errors.username && formik.touched.username && (
+                        <div className="error-text">{formik.errors.username}</div>
+                    )}
 
-                {/* Submit Button */}
-                <button type="submit" className="cart-btn" disabled={loading}>
-                    {loading ? 'Logging in...' : 'Log In'}
-                </button>
+                    {/* Password */}
+                    {/* <label htmlFor="password" className="d-flex flex-column">
+                        Password
+                    </label> */}
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.password}
+                        placeholder="Enter your password"
+                    />
+                    {formik.errors.password && formik.touched.password && (
+                        <div className="error-text">{formik.errors.password}</div>
+                    )}
 
-                {/* Error Message */}
-                {error && <div className="error-message">{error}</div>}
-            </form>
+                    {/* Submit Button */}
+                    <button type="submit" className="cart-btn" disabled={loading}>
+                        {loading ? 'Logging in...' : 'Log In'}
+                    </button>
+
+                    {/* Error Message */}
+                    {error && <div className="error-text">{error}</div>}
+                </form>
+                <div className="text-start ">
+                    <h2>Welcome</h2>
+                    <h4>To</h4>
+                    <div className="d-flex gap-3 justify-content-center align-items-center">
+                        <h3>LootLY</h3>
+                        <a href='/'>
+                            <img src={logo} alt='logo' />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
--- src/components/SignUp/SignIn.jsx
+++ src/components/SignUp/SignIn.jsx
@@ -4,6 +4,8 @@ import SignInApis from "../Services/SignInServices";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginUser } from "../slice/AuthSlice";
+import logo from '../../assets/logo.png'
+
 
 function SignIn() {
     const {
@@ -45,71 +47,71 @@ function SignIn() {
 
 
     return (
-        <section className="SignUp-section p-5">
-            <div className="form-container">
+        <section className="SignUp-section p-4">
+            <div className="form-container d-flex justify-content-evenly align-content-center gap-5">
                 <form
                     onSubmit={handleSubmit(onSubmit)}
-                    className="d-flex flex-column justify-content-center align-items-center gap-4"
+                    className="d-flex flex-column justify-content-center align-items-center gap-2"
                 >
                     {/* Name Field */}
-                    <label className="d-flex flex-column">
-                        Full Name
-                        <input
-                            {...register("name", { required: "Name is required", maxLength: 20 })}
-                            placeholder="Enter your full name"
-                        />
-                        {errors.name && <span className="error-text">{errors.name.message}</span>}
-                    </label>
+                    {/* <label className="d-flex flex-column">
+                        Full Name */}
+                    {errors.name && <span className="error-text">{errors.name.message}</span>}
+                    <input
+                        {...register("name", { required: "Name is required", maxLength: 20 })}
+                        placeholder="Enter your full name"
+                    />
+                    {/* </label> */}
 
                     {/* Email Field */}
-                    <label className="d-flex flex-column">
-                        Email
-                        <input
-                            type="email"
-                            {...register("email", {
-                                required: "Email is required",
-                                pattern: {
-                                    value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                                    message: "Invalid email format",
-                                },
-                            })}
-                            placeholder="Enter your email"
-                        />
-                        {errors.email && <span className="error-text">{errors.email.message}</span>}
-                    </label>
+                    {/* <label className="d-flex flex-column">
+                        Email */}
+                    {errors.email && <span className="error-text">{errors.email.message}</span>}
+                    <input
+                        type="email"
+                        {...register("email", {
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                                message: "Invalid email format",
+                            },
+                        })}
+                        placeholder="Enter your email"
+                    />
+                    {/* </label> */}
 
                     {/* Password Field */}
-                    <label className="d-flex flex-column">
-                        Password
-                        <input
-                            type="password"
-                            {...register("password", {
-                                required: "Password is required",
-                                minLength: {
-                                    value: 6,
-                                    message: "Password must be at least 6 characters long",
-                                },
-                            })}
-                            placeholder="Enter your password"
-                        />
-                        {errors.password && <span className="error-text">{errors.password.message}</span>}
-                    </label>
+                    {/* <label className="d-flex flex-column">
+                        Password */}
+                    {errors.password && <span className="error-text">{errors.password.message}</span>}
+                    <input
+                        type="password"
+                        {...register("password", {
+                            required: "Password is required",
+                            minLength: {
+                                value: 6,
+                                message: "Password must be at least 6 characters long",
+                            },
+                        })}
+                        placeholder="Enter your password"
+                    />
+                    {/* </label> */}
 
                     {/* About Field */}
-                    <label className="d-flex flex-column">
-                        About
-                        <textarea
-                            {...register("about", {
-                                required: "About field is required",
-                                maxLength: {
-                                    value: 200,
-                                    message: "About section must be less than 200 characters",
-                                },
-                            })}
-                            placeholder="Tell us about yourself"
-                        />
-                        {errors.about && <span className="error-text">{errors.about.message}</span>}
-                    </label>
+                    {/* <label className="d-flex flex-column">
+                        About */}
+                    {errors.about && <span className="error-text">{errors.about.message}</span>}
+                    <textarea
+                        {...register("about", {
+                            required: "About field is required",
+                            maxLength: {
+                                value: 200,
+                                message: "About section must be less than 200 characters",
+                            },
+                        })}
+                        placeholder="Tell us about yourself"
+                    />
+                    {/* </label> */}
 
                     {/* Error Message */}
                     {error && <span className="error-text">{error}</span>}
@@ -118,10 +120,21 @@ function SignIn() {
                     <button type="submit" className="cart-btn" disabled={loading}>
                         {loading ? "Submitting..." : "Submit"}
                     </button>
-                    <NavLink to='/log-in'>
-                        <p>Already Have an Acoount? Log In</p>
+                    <NavLink to='/log-in' className="text-muted">
+                        <p>Already Have an Account? Log In</p>
                     </NavLink>
+
                 </form>
+                <div className="text-start ">
+                    <h2>Welcome</h2>
+                    <h4>To</h4>
+                    <div className="d-flex gap-3 justify-content-center align-items-center">
+                        <h3>LootLY</h3>
+                        <a href='/'>
+                            <img src={logo} alt='logo' />
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     );
